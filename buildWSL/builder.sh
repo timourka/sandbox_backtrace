b9bf21b27857604d867b21c81178da1b67a30fdc
Timourka <timourkot@gmail.com>
2025-06-27 13:44:09 +0400

    трассировка работает

--- buildWSL/builder.sh
+++ buildWSL/builder.sh
@@ -1,8 +1,3 @@
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 make -j1
-file sandbox
-readelf -S sandbox | grep debug
-addr2line -e ./sandbox 0x55af4bb3d78f
-./sandbox &
-pid=$!
-cat /proc/$pid/maps | grep sandbox
+./sandbox
--- main.cpp
+++ main.cpp
@@ -1,60 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstdint>
 #include <boost/stacktrace.hpp>
+#include <signal.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 void causeCrash() {
-    throw std::runtime_error("Simulated error");
+#ifdef _WIN32
+    RaiseException(
+        EXCEPTION_ACCESS_VIOLATION,
+        0,
+        0,
+        NULL
+    );
+#else
+    while (1);
+#endif
 }
 
 void nestedCall() {
     causeCrash();
 }
 
-std::uintptr_t get_base_address() {
-    std::ifstream maps("/proc/self/maps");
-    std::string line;
 
-    while (std::getline(maps, line)) {
-        if (line.find("r-xp") != std::string::npos && line.find("sandbox") != std::string::npos) {
-            std::istringstream iss(line);
-            std::string address_range;
-            iss >> address_range;
-
-            size_t dash_pos = address_range.find('-');
-            if (dash_pos != std::string::npos) {
-                std::string base_str = address_range.substr(0, dash_pos);
-                return std::stoull(base_str, nullptr, 16);
-            }
-        }
-    }
-    return 0;
+static void signalHandler(int signal)
+{
+    std::cerr << boost::stacktrace::stacktrace();
 }
 
 int main() {
-    try {
-        throw std::runtime_error("Simulated error");
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Caught exception: " << e.what() << "\n";
-        std::cerr << "Stack trace:\n";
-
-        std::uintptr_t base = get_base_address();
-        for (auto& frame : boost::stacktrace::stacktrace()) {
-            auto addr = reinterpret_cast<std::uintptr_t>(frame.address());
-            std::uintptr_t offset = addr - base;
-
-            /*std::cerr << "address:" << frame.address()
-                << " offset:0x" << std::hex << offset << std::dec
-                << "\tname:" << frame.name()
-                << "\tsource:" << frame.source_file()
-                << ":" << frame.source_line() << "\n";*/
-            
-            void* ptr = reinterpret_cast<void*>(offset);
-            boost::stacktrace::frame f(ptr);
-            std::cerr << frame << "\n";
-            std::cerr << f << "\n";
-        }
-    }
+    signal(SIGABRT, &signalHandler);
+    signal(SIGTERM, &signalHandler);
+    signal(SIGINT, &signalHandler);
+    signal(SIGSEGV, &signalHandler);
+    nestedCall();
     return 0;
 }
